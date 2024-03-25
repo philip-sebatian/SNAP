f495bef85fb396d7e3e897a40b7465f17a73f96d
philip-sebatian <philipvacha14@gmail.com>
2024-03-25 23:17:45 +0530

    made file compatable

--- main.c
+++ main.c
@@ -2,12 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "file.c"
 
 int top=0;
+int get_count(char *);
+char **get_files(char *);
 
+int **get_dp_file(char *, char *);
+char *delete_letter(char *, int);
+char *add_letter(char *, int, char);
 typedef struct act{
   char action;
-  char letter ;
+  char *letter ;
   int idx;
 }actions;
 actions action_stack[1024];
@@ -83,14 +89,61 @@ int ** get_dp(char * word1,char *word2){
 
 }
 
+
+int ** get_dp_file(char *word1,char *word2){
+  int l1 = get_count(word1);
+  int l2 = get_count(word2);
+
+
+  char ** w1=get_files(word1);
+  char ** w2=get_files(word2);
+  //dynamically allocates a 2d array for computation of dp
+  int **dp=(int**)malloc((l1+1)*sizeof(int *));
+  for(int i =0 ; i< l1+1;i++){
+    dp[i]=(int*)malloc((l2+1)*sizeof(int *));
+  }
+  //fills the base cases in the dp table and also fills other values with zeroes
+  for(int i =0 ; i < l1+1;i++){
+    for(int j=0 ; j < l2+1;j++){
+      dp[i][j]=0;
+    }
+  }
+  for(int i=0 ; i<l1+1;i++){
+    dp[i][0]=i;
+  }
+  for(int i =0; i <l2+1;i++){
+    dp[0][i]=i;
+  }
+  
+  //the dp loop for calculating distance of the strings
+  for(int i =1; i<l1+1;i++){
+    for(int j=1 ; j<l2+1;j++){
+      if(strcmp(w1[i-1],w2[j-1])==0){
+
+        dp[i][j]=dp[i-1][j-1];
+      }
+      else{
+        if(dp[i-1][j]<dp[i][j-1]){
+          dp[i][j]=dp[i-1][j]+1;
+        }
+        else{
+          dp[i][j]=dp[i][j-1]+1;
+        }
+      }
+    }
+  }
+  return dp;
+
+}
+
 //for tracing actions on action stack array dynamically alocated by actions
-actions * trace_actions(actions * action_stack,int **dp,char * word1,char * word2){
-  int i =strlen(word1);
+actions * trace_actions(actions * action_stack,int **dp,char **word1,char **word2,char *v1,char *v2){
+  int i =get_count(v1);
   actions temp_action ;
-  int j =strlen(word2);
+  int j =get_count(v2);
   int curr =i;
   while(i>0 && j>0){
-    if(word1[i-1]==word2[j-1]){
+    if(strcmp(word1[i-1],word2[j-1])==0){
       i--;
       j--;
     }
@@ -100,7 +153,7 @@ actions * trace_actions(actions * action_stack,int **dp,char * word1,char * word
         temp_action.letter=word1[i-1];
         temp_action.idx=i-1;
         action_stack[top]=temp_action;
-        printf("%s-%c\n","remove",word1[i-1]);
+        printf("%s-%s\n","remove",word1[i-1]);
         i--;
 
         top++;
@@ -114,7 +167,7 @@ actions * trace_actions(actions * action_stack,int **dp,char * word1,char * word
           temp_action.letter=word2[j-1];
           temp_action.idx=i;
           action_stack[top]=temp_action;
-          printf("%s-%c\n","add",word2[j-1]);
+          printf("%s-%s\n","add",word2[j-1]);
           
           j--;
           top++;
@@ -127,7 +180,7 @@ actions * trace_actions(actions * action_stack,int **dp,char * word1,char * word
     temp_action.idx=i-1;
     temp_action.letter=word1[i-1];
     action_stack[top]=temp_action;
-    printf("%s-%c\n","remove",word1[i-1]);
+    printf("%s-%s\n","remove",word1[i-1]);
     top++;
     
     i--;
@@ -138,7 +191,7 @@ actions * trace_actions(actions * action_stack,int **dp,char * word1,char * word
     temp_action.idx=i;
     temp_action.letter=word2[j-1];
     action_stack[top]=temp_action;
-    printf("%s-%c\n","add",word2[j-1]);
+    printf("%s-%s\n","add",word2[j-1]);
     top++;
     
     j--;
@@ -150,9 +203,11 @@ int main(int argc, char * argv[]){
   actions *action_stack=get_action_stack(1024);
   char * word1 = argv[1];
   char * word2 = argv[2];
-  int l1 = strlen(word1);
-  int l2 = strlen(word2);
-  int **dp=get_dp(word1,word2);
+  char **f1= get_files(word1);
+  char **f2=get_files(word2);
+  int l1 = get_count(word1);
+  int l2 = get_count(word2);
+  int **dp=get_dp_file(word1,word2);
   
   printf("\n\n");
   for(int i =0 ; i < l1+1;i++){
@@ -162,25 +217,25 @@ int main(int argc, char * argv[]){
     printf("\n");
   }
   
-  trace_actions(action_stack,dp,word1,word2);
+  trace_actions(action_stack,dp,f1,f2,word1,word2);
   printf("\n\n");
  int curr =0 ;
- while(curr<top){
+ /*while(curr<top){
   if(action_stack[curr].action=='r'){
-   printf("%s %c %s %d\n","remove",action_stack[curr].letter,"at index",action_stack[curr].idx);
+   printf("%s %s %s %d\n","remove",action_stack[curr].letter,"at index",action_stack[curr].idx);
   
    word1=delete_letter(word1,action_stack[curr].idx);
    
    
   }else{
-   printf("%s %c %s %d\n","add",action_stack[curr].letter,"at index",action_stack[curr].idx);
+   printf("%s %s %s %d\n","add",action_stack[curr].letter,"at index",action_stack[curr].idx);
    
    word1=add_letter(word1,action_stack[curr].idx,action_stack[curr].letter);
    
   }curr++;
   printf("%s\n",word1);
   
- }
+ }*/
  
  
  
