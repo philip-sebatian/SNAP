48a9ddf3ba6f84e3a8d8a7772fee0be3b78ad53e
philip-sebatian <philipvacha14@gmail.com>
2024-03-26 23:25:32 +0530

    asdjdws

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #include<bits/stdc++.h>
+#include <fstream>
+//class for the actions
 
 
 class actions{
     
     public:
-    char letter ;
+    std::string letter ;
     int idx ;
     std::string action;
-        actions(char letter,int idx,std::string action){
+        actions(std::string letter,int idx,std::string action){
             this->letter=letter;
             this->idx=idx;
             this->action=action;
         }
 };
 
-
-std::stack<actions> stk;
-std::vector<std::vector<int>> calc(std::string s1,std::string s2){
+//deque to store the actions
+std::deque<actions> stk;
+std::vector<std::string> add_line(std::vector<std::string>&,std::string ,int );
+std::vector<std::string> delete_line(std::vector<std::string>&,int);
+//calculates the dp for diff
+std::vector<std::vector<int>> calc(std::vector<std::string> s1,std::vector <std::string> s2){
     std::vector<std::vector<int>>dp(s1.size()+1,std::vector<int>(s2.size()+1,0));
     for(int i=0 ; i < s1.size()+1;i++){
         dp[i][0]=i;
@@ -49,9 +54,12 @@ std::vector<std::vector<int>> calc(std::string s1,std::string s2){
     
 }
 
-void get_actions(std::vector<std::vector<int>>&dp,std::string s1,std::string s2){
-    int i =s1.size()+1;
-    int j =s2.size()+1;
+
+//gets the actions needed to be performed
+
+void get_actions(std::vector<std::vector<int>>&dp,std::vector<std::string> &s1,std::vector<std::string> &s2){
+    int i =s1.size();
+    int j =s2.size();
     while(i>0 && j>0){
         if(s1[i-1]==s2[j-1]){
             i--;
@@ -59,31 +67,97 @@ void get_actions(std::vector<std::vector<int>>&dp,std::string s1,std::string s2)
         }
         else if(dp[i-1][j]+1==dp[i][j]){
             
-            stk.push(actions(s1[i-1],i-1,"remove"));
+            stk.push_back(actions(s1[i-1],i-1,"remove"));
             i--;
         }
         else{
-            stk.push(actions(s2[j-1],i,"add"));
+            stk.push_back(actions(s2[j-1],i,"add"));
             j--;
         }
+        
     }
     while(i>0){
-        stk.push(actions(s1[i-1],i-1,"remove"));
+        stk.push_back(actions(s1[i-1],i-1,"remove"));
         i--;
     }
     while(j>0){
-        stk.push(actions(s2[j-1],i,"add"));
+        stk.push_back(actions(s2[j-1],i,"add"));
         j--;
     }
     while(!stk.empty()){
-        std::cout<<stk.top().action<<" "<<stk.top().letter<<" at index-"<<stk.top().idx<<std::endl;
-        stk.pop();
+        std::cout<<stk.front().action<<" "<<stk.front().letter<<" at index-"<<stk.front().idx<<std::endl;
+        if(stk.front().action=="remove"){
+            delete_line(s1,stk.front().idx);
+        }
+        else{
+            add_line(s1,stk.front().letter,stk.front().idx);
+        }
+        stk.pop_front();
     }
+}
+
+
+//gets the content of the files of a file as array of strings
+std::vector<std::string> get_content(std::string fname){
+    std:: fstream ff ;
+    ff.open(fname,std::ios::out|std::ios::in);
+    std::string str ;
+    std::vector <std::string>lines;
+    while(getline(ff,str)){
+    
+        std::cout<<str<<std::endl;
+        lines.push_back(str);
+   }
+   return lines;
+
+}
+std::vector<std::string> add_line(std::vector<std::string>&file_content,std::string s1,int idx){
+    std::vector<std::string> temp;
+    bool flg=false;
+    for(int i =0 ; i < file_content.size();i++){
+        if(i==idx){
+            
+            if(!flg){
+                temp.push_back(s1);
+                i--;
+                }
+            flg=true;
+            continue;
+        }
+        temp.push_back(file_content[i]);
+    }
+    file_content=temp;
+    return file_content;
+}
+
+std::vector<std::string> delete_line(std::vector<std::string>&file_content,int idx){
+    std::vector<std::string> temp;
+    for(int i =0 ; i < file_content.size();i++){
+        if(i==idx){
+            continue;
+        }
+        temp.push_back(file_content[i]);
+    }
+    file_content=temp;
+    return file_content;
 }
 
 int main()
 {
-   std :: vector<std::vector<int>> dp = calc("hello","hi");
-   get_actions(dp,"hello","hi");
+   
+   
+   std ::string str ;
+   std::vector<std::string> lines1;
+   std::vector<std::string> lines2;
+   lines1=get_content("hello.txt");
+   lines2=get_content("new.txt");
+   std::vector<std::vector<int>> dp =calc(lines1,lines2);
+   get_actions(dp,lines1,lines2);
+   for(auto i : lines1){
+    std::cout<<i<<std::endl;
+   }
+   
+   
+
 
 }
